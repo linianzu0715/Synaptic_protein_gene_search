68b4100b41c86ceb06c9a193871dd8cdef4ea038
linianzu0715 <55205912+linianzu0715@users.noreply.github.com>
2020-08-15 09:58:09 +0800

    delete

--- server/redis.bat
+++ server/redis.bat
@@ -1,4 +1,4 @@
 C:
-cd C:\Users\59584\Desktop\ppi\X-SpringBoot-master3\X-SpringBoot-master\\server\redis
+cd C:\Users\59584\Desktop\ppi\Synaptic_protein_gene_search\\server\redis
 redis-server.exe redis.windows.conf
 exit
